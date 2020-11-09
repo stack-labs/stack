5c56b4b6638f6d1532c72eb1aad872193a8dfd9a
Allenxuxu <120582243@qq.com>
2020-11-09 20:35:28 +0800

    remove copy backup file

--- config/storage/file/file.go
+++ config/storage/file/file.go
@@ -1,8 +1,6 @@
 package file
 
 import (
-	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -33,16 +31,6 @@ func (f *file) Write(content []byte) (err error) {
 	if err := os.MkdirAll(filepath.Dir(f.file), _perm); err != nil {
 		return err
 	}
-	// create backup file
-	exist, err := existFile(f.file)
-	if err != nil {
-		return err
-	}
-	if exist {
-		if err := copyFile(f.file, fmt.Sprintf("%s_backup", f.file)); err != nil {
-			return err
-		}
-	}
 
 	if err := ioutil.WriteFile(f.file, content, _perm); err != nil {
 		return err
@@ -53,35 +41,6 @@ func (f *file) Write(content []byte) (err error) {
 
 func (f *file) Load() (config []byte, err error) {
 	return ioutil.ReadFile(f.file)
-}
-
-func copyFile(source string, dest string) error {
-	src, err := os.Open(source)
-	if err != nil {
-		return err
-	}
-	defer src.Close()
-
-	dst, err := os.Create(dest)
-	if err != nil {
-		return err
-	}
-	defer dst.Close()
-
-	_, err = io.Copy(dst, src)
-	if err != nil {
-		return err
-	}
-
-	info, err := os.Stat(source)
-	if err != nil {
-		err = os.Chmod(dest, info.Mode())
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
 
 func existFile(file string) (bool, error) {

cc92827af813c3d671dfe024ace6602fcda1e5d2
shu xian <i@shux.me>
2021-01-29 22:02:52 +0800

    add logrus

---
+++ plugin/logger/logrus/go.mod
@@ -0,0 +1,10 @@
+module github.com/stack-labs/stack-rpc/plugin/logger/logrus
+
+go 1.14
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/sirupsen/logrus v1.4.2
+	github.com/stack-labs/stack-rpc v1.0.1-rc1
+	gopkg.in/yaml.v2 v2.3.0
+)
---
+++ plugin/logger/logrus/level_hooks.go
@@ -0,0 +1,62 @@
+package logrus
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/stack-labs/stack-rpc/logger"
+	ls "github.com/stack-labs/stack-rpc/plugin/logger/logrus/logrus"
+	"github.com/stack-labs/stack-rpc/plugin/logger/logrus/lumberjack.v2"
+)
+
+func prepareLevelHooks(opts logger.PersistenceOptions, l ls.Level) ls.LevelHooks {
+	hooks := make(ls.LevelHooks)
+
+	for _, level := range ls.AllLevels {
+		if level <= l {
+			fileName := fmt.Sprintf("%s%s%s.log", opts.Dir, pathSeparator, level.String())
+			logger.Infof("level %s logs to file: %s", level.String(), fileName)
+			// todo default options?
+			maxBackups := 14
+			if opts.MaxFileSize != 0 {
+				maxBackups = opts.MaxBackupSize / opts.MaxFileSize
+			}
+
+			hook := &PersistenceLevelHook{
+				Writer: &lumberjack.Logger{
+					Filename:   fileName,
+					MaxSize:    opts.MaxFileSize,
+					MaxBackups: maxBackups,
+					MaxAge:     opts.MaxBackupKeepDays,
+					Compress:   true,
+					BackupDir:  opts.BackupDir,
+				},
+				Fired:  true,
+				levels: []ls.Level{level},
+			}
+
+			hooks[level] = []ls.Hook{hook}
+		}
+	}
+
+	return hooks
+}
+
+type PersistenceLevelHook struct {
+	Writer io.Writer
+	Fired  bool
+	levels []ls.Level
+}
+
+func (hook *PersistenceLevelHook) Levels() []ls.Level {
+	return hook.levels
+}
+
+func (hook *PersistenceLevelHook) Fire(entry *ls.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+	_, err = hook.Writer.Write([]byte(line))
+	return err
+}

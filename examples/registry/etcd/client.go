fbc444a8b18e0ba0c2bf49030fdeee80a91fe719
shu xian <i@shux.me>
2021-01-30 23:51:43 +0800

    add examples

---
+++ examples/registry/etcd/client.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+
+	"github.com/stack-labs/stack-rpc"
+	proto "github.com/stack-labs/stack-rpc/examples/proto/service/rpc"
+	"github.com/stack-labs/stack-rpc/logger"
+
+	_ "github.com/stack-labs/stack-rpc/plugin/registry/etcd"
+)
+
+func main() {
+	// 定义服务，可以传入其它可选参数
+	service := stack.NewService(stack.Name("stack.rpc.client"))
+	service.Init()
+
+	// 创建客户端
+	greeter := proto.NewGreeterService("stack.rpc.registry.etcd", service.Client())
+
+	// 调用greeter服务
+	rsp, err := greeter.Hello(context.TODO(), &proto.HelloRequest{Name: "StackLabs"})
+	if err != nil {
+		logger.Fatal(err)
+		return
+	}
+
+	// 打印响应结果
+	logger.Info(rsp.Greeting)
+}
---
+++ examples/registry/etcd/server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/stack-labs/stack-rpc"
+	proto "github.com/stack-labs/stack-rpc/examples/proto/service/rpc"
+	"github.com/stack-labs/stack-rpc/logger"
+
+	_ "github.com/stack-labs/stack-rpc/plugin/registry/etcd"
+)
+
+// 服务类
+type Greeter struct{}
+
+// 实现proto中的Hello接口
+func (g Greeter) Hello(ctx context.Context, req *proto.HelloRequest, rsp *proto.HelloResponse) error {
+	rsp.Greeting = "Hello! " + req.Name
+	return nil
+}
+
+func main() {
+	// 实例化服务，并命名为stack.rpc.registry.etcd
+	service := stack.NewService(
+		stack.Name("stack.rpc.registry.etcd"),
+		stack.RegisterTTL(10*time.Second),
+		stack.RegisterInterval(3*time.Second),
+	)
+	// 初始化服务
+	service.Init()
+
+	// 将Greeter注册到服务上
+	proto.RegisterGreeterHandler(service.Server(), new(Greeter))
+
+	// 运行服务
+	if err := service.Run(); err != nil {
+		logger.Error(err)
+	}
+}
